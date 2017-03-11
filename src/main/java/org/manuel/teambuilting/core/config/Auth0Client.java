4848e2974c7184c2d4296bde188077e608298461
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-03-11 23:45:40 +0100

    TeamBuilting: Refactoring and adding integration tests

--- src/main/java/org/manuel/teambuilting/core/config/Auth0Client.java
+++ src/main/java/org/manuel/teambuilting/core/config/Auth0Client.java
@@ -5,6 +5,8 @@ import com.auth0.authentication.AuthenticationAPIClient;
 import com.auth0.authentication.result.UserProfile;
 import com.auth0.request.Request;
 import com.auth0.spring.security.api.Auth0JWTToken;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,6 +25,7 @@ public class Auth0Client {
         this.client = this.auth0.newAuthenticationAPIClient();
     }
 
+    @Cacheable
     public UserProfile getUser(final Auth0JWTToken token) {
         final Request<UserProfile> request = client.tokenInfo(token.getJwt());
         return request.execute();
