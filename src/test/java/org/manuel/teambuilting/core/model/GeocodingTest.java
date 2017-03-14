4b4801073701b36b42e9027f51bb536525b842f7
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-03-14 11:55:31 +0100

    TeamBuiltingCore: Adding google api,

--- src/test/java/org/manuel/teambuilting/core/model/GeocodingTest.java
+++ src/test/java/org/manuel/teambuilting/core/model/GeocodingTest.java
@@ -3,13 +3,6 @@ package org.manuel.teambuilting.core.model;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.BeanDescription;
-import com.fasterxml.jackson.databind.DeserializationConfig;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
 import com.google.maps.model.AddressComponent;
 
 import java.io.IOException;
@@ -55,25 +48,6 @@ public class GeocodingTest {
 				return true;
 			}
 
-		};
-	}
-
-
-	private BeanDeserializerModifier customDeserializers() {
-		return new BeanDeserializerModifier() {
-			@Override
-			public JsonDeserializer<Enum> modifyEnumDeserializer(DeserializationConfig config,
-				final JavaType type,
-				BeanDescription beanDesc,
-				final JsonDeserializer<?> deserializer) {
-				return new JsonDeserializer<Enum>() {
-					@Override
-					public Enum deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-						Class<? extends Enum> rawClass = (Class<Enum<?>>) type.getRawClass();
-						return Enum.valueOf(rawClass, jp.getValueAsString().toUpperCase());
-					}
-				};
-			}
 		};
 	}
 
