28aea19bc081dd03dd0ed43e9e07785cef1d8e08
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2017-05-28 13:42:52 +0200

    TeamBuiltingUsers: Using auth0 api java

--- src/main/java/org/manuel/teambuilting/users/controllers/query/UserQueryController.java
+++ src/main/java/org/manuel/teambuilting/users/controllers/query/UserQueryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
-public class UserController {
+public class UserQueryController {
 
 	private final UserQueryService queryService;
 
